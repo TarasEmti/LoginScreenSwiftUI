02bc52cc97399a59a9a3d1ec45d96c3785d9a555
Taras Minin <taras.minin@connect.club>
2021-05-23 18:32:04 +0300

    Initial commit

---
+++ loginApp/loginApp/loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  loginApp
+//
+//  Created by Тарас Минин on 23/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
